e6096fd4f74274d913f36d8fdce6fac905d491b0
AussieKing <126050763+AussieKing@users.noreply.github.com>
2023-04-28 13:57:56 +1000

    modified HTML, added cdn link to tailwind, testing funcitonality ok, changed named of tailwind.js

---
+++ tailwind.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["index.html", "search.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+//! We are not using tailwind with react
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+};
+
+
