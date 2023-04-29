1c4ff5c946f7ce4821d238186e832adea8629ca6
AussieKing <126050763+AussieKing@users.noreply.github.com>
2023-04-29 21:57:45 +1000

    started the styling of the Homepage with tailwind and working on the layout

--- tailwind.js
+++ tailwind.js
@@ -18,4 +18,34 @@ tailwind.config = {
   },
 };
 
+// // LANDING PAGE
+// const colors = require();
+// module.exports = {
+//   jit: true,
+//   purge: ["./src/**/*.html", "./src/**/*.js"],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     colors: {
+//       red: colors.red,
+//       white: colors.white,
+//       gray: colors.trueGray,
+//       black: colors.black,
+//     }
+//     screens:{  //! setting page for 2 screen sizes (small and large)
+//       'sm': '300px',
+//       'lg': '760px',
+//     }
+//     extends: {  //! extending the background images
+//       backgroundimage: theme => ({
+//         'hero': "url('/img/hero-pattern.svg')",
+//         'action': "url('/img/action-pattern.svg')",
+//     })
+//   },
+// variants: {
+//   extend: {},
+// },
+// plugins: [
+
+// ],
+// }
 
