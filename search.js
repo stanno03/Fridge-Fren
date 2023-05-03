152cf34167426d3400cd4706b6739abf44774b84
stanno03 <danno_41@hotmail.com>
2023-05-04 02:03:44 +1000

    added api call to random fact generator

--- search.js
+++ search.js
@@ -10,7 +10,31 @@ toggleBtn.onclick = () => {
 
 var apiKey = "&apiKey=f9d4380259ef478d8d7196ce99308f41";
 
+// 
+
+
+
+
 $(document).ready(function() {
+  $.get("https://api.spoonacular.com/recipes/search?random&number=6&apiKey=f9d4380259ef478d8d7196ce99308f41", function(data) {
+    // empty the search results div
+    $(".search-results").empty();
+    
+    // loop through the search results and display them in the search results div
+    for (var i = 0; i < data.results.length; i++) {
+      var result = data.results[i];
+      var item = '<div class="item w-screen bg-cover flex-1 max-w-fit rounded-lg bg-green-300 p-3.5 overflow-hidden">' +
+        '<img class="w-full h-72 rounded" src="https://spoonacular.com/recipeImages/' + result.image + '" alt="' + result.title + '">' +
+        '<div class="flex items-center justify-between">' +
+          '<h1 class="title text-blue-900 mt-1.25 ml-2.5 text-xl">' + result.title + '</h1>' +
+          '<a class="rounded-full view-button text-center w-32 pt-4 py-0 bg-green-400 text-sm no-underline mt-8 self-start;" href="' + result.sourceUrl + '">View Recipe</a>' +
+        '</div>' +
+        '<a class="rounded-full view-button text-center w-32 pt-4 py-0 bg-green-400 text-sm no-underline mt-8 self-start;" href="' + result.sourceUrl + '">View Recipe</a>' +
+        '</div>' 
+      $(".search-results").append(item);
+    }
+  })
+
   // function to search for recipes
   function searchRecipes() {
     // get the search query from the input field
@@ -25,7 +49,7 @@ $(document).ready(function() {
       for (var i = 0; i < data.results.length; i++) {
         var result = data.results[i];
         var item = '<div class="item w-screen bg-cover flex-1 max-w-fit rounded-lg bg-green-300 p-3.5 overflow-hidden">' +
-          '<img class="w-full h-72 rounded" src="' + result.image + '" alt="' + result.title + '">' +
+          '<img class="w-full h-72 rounded" src="https://spoonacular.com/recipeImages/' + result.image + '" alt="' + result.title + '">' +
           '<div class="flex items-center justify-between">' +
             '<h1 class="title text-blue-900 mt-1.25 ml-2.5 text-xl">' + result.title + '</h1>' +
             '<a class="view-button text-center w-32 pt-4 py-0 bg-green-400 text-sm no-underline mt-8 self-start" href="' + result.sourceUrl + '">View Recipe</a>' +
@@ -39,6 +63,8 @@ $(document).ready(function() {
   
   // bind the search function to the search button
   $("#btn").click(function() {
+  //  var url = "https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=f9d4380259ef478d8d7196ce99308f41"
+
     searchRecipes();
   });
   
@@ -47,6 +73,24 @@ $(document).ready(function() {
     if (event.which == 13) {
       searchRecipes();
     }
+
+    $("#foodfact").on('click', function(){
+      $.ajax({
+        async: false,
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/facts?limit=1',
+        headers: { 'x-api-key': 'MnwIvd3jpy3FJQnXtFKRjA==cB7H5HsSlg1CxnDF'},
+        
+        success: function(result) {
+          window.alert(result)
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+
+
+    })
   });
 });
 
